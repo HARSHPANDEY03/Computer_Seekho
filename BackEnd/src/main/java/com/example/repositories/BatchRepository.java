3ec6f7e4cf5d499f9e7e7b2d0e84774997b9837f
Ashish <jakiewarrior99@gmail.com>
2026-07-24 13:16:11 +0000

    added batch repository

---
+++ BackEnd/src/main/java/com/example/repositories/BatchRepository.java
@@ -0,0 +1,13 @@
+package com.example.repositories;
+
+import com.example.entities.Batch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BatchRepository extends JpaRepository<Batch, Integer> {
+    List<Batch> findByCourseCourseId(Integer courseId);
+    List<Batch> findByCourseCourseIdAndBatchIsActiveTrue(Integer courseId);
+}
