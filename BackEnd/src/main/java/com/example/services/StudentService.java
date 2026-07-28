4f4ca0b62e9f501599704965d5a76bc50008a65b
Vaishnavi Hambir <vaishnavihambir055@gmail.com>
2026-07-28 21:00:22 +0530

    Format updateStudent method signature for readability

--- BackEnd/src/main/java/com/example/services/StudentService.java
+++ BackEnd/src/main/java/com/example/services/StudentService.java
@@ -20,5 +20,7 @@ public interface StudentService {
             String name,
             Long mobile);
 
-    StudentResponse updateStudent(  Integer studentId,   StudentRequest request);
+  StudentResponse updateStudent(
+            Integer studentId,
+            StudentRequest request);
 }
