d61888417b49dd2bde699f832bf608055fd2b603
Jayesh Sonawane <sonawanejayesh947@gmail.com>
2026-08-03 13:47:57 +0530

    folder structure updated

---
+++ FrontEnd/Computer_seekho/src/api/auth.js
@@ -0,0 +1,9 @@
+import { client } from './client';
+
+export const login = (staffUsername, staffPassword) =>
+  client.post('/api/auth/login', { staffUsername, staffPassword }).then((r) => r.data);
+
+export const changePassword = (payload) =>
+  client.put('/api/auth/change-password', payload).then((r) => r.data);
+
+export const logout = () => client.post('/api/auth/logout').then((r) => r.data);
